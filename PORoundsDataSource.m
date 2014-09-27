8530d959b6c6060d935c51cdeeaadd7c51397667
Emily Hoehne <emilyhoehne@theglutconomist.home>
2014-09-27 07:35:33 -0600

    Initial issues

--- PORoundsDataSource.m
+++ PORoundsDataSource.m
@@ -26,9 +26,15 @@ static NSString * const CurrentRoundKey= @"CurrentRound";
 -(NSInteger)tableView:(UITableView*)tableView numberOfRowsInSection:(NSInteger)section {
     return [[self times] count];
 }
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    self.currentRound = indexPath.row;
+    [self postMinutes];
+}
+    
+    
 -(UITableViewCell*)tableView:(UITableView*)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
-   UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+   UITableViewCell *cell = [UITableViewCell dequeueReusableCellWithIdentifier:@"cell"];
     cell.textLabel.text = [NSString stringWithFormat:@"Round %ld - %@min", indexPath.row + 1, [[[self times] objectAtIndex: indexPath.row] stringValue]];
     return cell;
 }
@@ -38,14 +44,30 @@ static NSString * const CurrentRoundKey= @"CurrentRound";
 - (NSArray *)times {
     return @[@25, @5, @25, @5, @25, @5, @25, @15];
 }
+- (void)postMinutes {
+    [[NSNotificationCenter defaultCenter]postNotificationName: RoundCompleteNotification object:self];
+    
+    
+}
+- (void)endRound: (NSNotification *)notification {
+    self.currentRound++;
+    if (self.currentRound == [[self times] count]) {
+        self.currentRound = 0;
+    }
+    [self selectCurrentRound];
+    [self postMinutes];
+}
 - (void)setCurrentRound:(NSInteger)currentRound {
     if (currentRound >= [[self times] count] ) {
-        _currentRound = 0;
+        self.currentRound = 0;
     } else {
-        _currentRound = currentRound;
+        self.currentRound = currentRound;
     }
     [[NSUserDefaults standardUserDefaults] setValue:@(currentRound) forKey:CurrentRoundKey];
     [[NSUserDefaults standardUserDefaults] synchronize];
+}
+- (void) selectCurrentRound {
+    NSIndexPath *indexPath = [NSIndexPath indexPathForItem:self.currentRound inSection:0];
 }
 
 
